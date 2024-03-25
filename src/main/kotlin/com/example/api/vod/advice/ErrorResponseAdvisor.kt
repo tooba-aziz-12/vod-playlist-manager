aeee504aaa6a9dad7c2902f17f95e1e0d0a2b251
toobaaziz <toobaaziz@digitalhire.com>
2024-03-25 09:06:23 +0500

    added advices for all custom exceptions for standardised error response

--- src/main/kotlin/com/example/api/vod/advice/ErrorResponseAdvisor.kt
+++ src/main/kotlin/com/example/api/vod/advice/ErrorResponseAdvisor.kt
@@ -2,6 +2,7 @@ package com.example.api.vod.advice
 
 import com.example.api.vod.constant.ErrorCode
 import com.example.api.vod.dto.ErrorMessageDto
+import com.example.api.vod.exception.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.util.stream.Collectors
 
 @RestControllerAdvice(basePackages = ["com.example.api.vod"])
-
 class ErrorResponseAdvisor {
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
@@ -34,5 +34,77 @@ class ErrorResponseAdvisor {
         )
 
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(
+        FailedToSavePlaylistException::class
+    )
+    fun handleFailedToSavePlaylistException(ex: FailedToSavePlaylistException): ResponseEntity<ErrorMessageDto> {
+        val errorMessage = ErrorMessageDto(
+            HttpStatus.INTERNAL_SERVER_ERROR.name,
+            mutableListOf(ex.message),
+            ex.errorCode,
+        )
+        return ResponseEntity(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(
+        FailedToSavePlaylistItemException::class
+    )
+    fun handleFailedToSavePlaylistItemException(ex: FailedToSavePlaylistItemException): ResponseEntity<ErrorMessageDto> {
+        val errorMessage = ErrorMessageDto(
+            HttpStatus.INTERNAL_SERVER_ERROR.name,
+            mutableListOf(ex.message),
+            ex.errorCode,
+        )
+        return ResponseEntity(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(
+        FailedToFindPlaylistException::class
+    )
+    fun handleFailedToFindPlaylistException(ex: FailedToFindPlaylistException): ResponseEntity<ErrorMessageDto> {
+        val errorMessage = ErrorMessageDto(
+            HttpStatus.INTERNAL_SERVER_ERROR.name,
+            mutableListOf(ex.message),
+            ex.errorCode,
+        )
+        return ResponseEntity(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(
+        FailedToDeletePlaylistException::class
+    )
+    fun handleFailedToDeletePlaylistException(ex: FailedToDeletePlaylistException): ResponseEntity<ErrorMessageDto> {
+        val errorMessage = ErrorMessageDto(
+            HttpStatus.INTERNAL_SERVER_ERROR.name,
+            mutableListOf(ex.message),
+            ex.errorCode,
+        )
+        return ResponseEntity(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(
+        PlaylistNotFoundException::class
+    )
+    fun handlePlaylistNotFoundException(ex: PlaylistNotFoundException): ResponseEntity<ErrorMessageDto> {
+        val errorMessage = ErrorMessageDto(
+            HttpStatus.NOT_FOUND.name,
+            mutableListOf(ex.message),
+            ex.errorCode,
+        )
+        return ResponseEntity(errorMessage, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(
+        PlaylistItemNotFoundException::class
+    )
+    fun handlePlaylistItemNotFoundException(ex: PlaylistItemNotFoundException): ResponseEntity<ErrorMessageDto> {
+        val errorMessage = ErrorMessageDto(
+            HttpStatus.NOT_FOUND.name,
+            mutableListOf(ex.message),
+            ex.errorCode,
+        )
+        return ResponseEntity(errorMessage, HttpStatus.NOT_FOUND)
     }
 }
