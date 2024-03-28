496f1ae26520f0682a32812b60dc2be29f0255f7
toobaaziz <toobaaziz@digitalhire.com>
2024-03-28 12:19:25 +0500

    added test cases for reorder item api

--- src/test/kotlin/com/example/api/vod/service/PlayListItemServiceTest.kt
+++ src/test/kotlin/com/example/api/vod/service/PlayListItemServiceTest.kt
@@ -1,5 +1,9 @@
 package com.example.api.vod.service
 
+import com.example.api.vod.exception.FailedToSavePlaylistException
+import com.example.api.vod.exception.FailedToSavePlaylistItemException
+import com.example.api.vod.exception.PlaylistItemNotFoundException
+import com.example.api.vod.exception.PlaylistNotFoundException
 import com.example.api.vod.fixture.PlaylistFixture
 import com.example.api.vod.fixture.PlaylistFixture.Companion.playListReorderItemDto
 import com.example.api.vod.fixture.PlaylistFixture.Companion.playlist
@@ -15,7 +19,9 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentCaptor
 import org.mockito.Captor
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.times
 import org.mockito.kotlin.whenever
 import java.util.*
 
@@ -71,9 +77,59 @@ class PlayListItemServiceTest {
 
             playlistItemService.reorderItemsInPlaylist(requestDto)
 
+            Mockito.verify(playlistRepository, times(1)).findById(playListReorderItemDto.playlistId)
+            Mockito.verify(playlistRepository, times(1)).save(playlistCaptor.capture())
+
             val actualInvocationOfSave = playlistCaptor.value
 
             Assertions.assertEquals(2, actualInvocationOfSave.items[0].sequence)
+        }
+
+        @Test
+        fun throwCustomExceptionIfNotFound(){
+
+            whenever(playlistRepository.findById(playListReorderItemDto.playlistId)).thenReturn(Optional.empty())
+
+            try {
+                playlistItemService.reorderItemsInPlaylist(playListReorderItemDto)
+
+            }catch (ex: Exception){
+
+                Assertions.assertEquals(PlaylistNotFoundException::class.java, ex.javaClass)
+            }
+
+            Mockito.verify(playlistRepository, times(1)).findById(playListReorderItemDto.playlistId)
+            Mockito.verify(playlistRepository, times(0)).save(playlistCaptor.capture())
+        }
+
+        @Test
+        fun throwCustomExceptionIfItemNotFound(){
+
+            whenever(playlistRepository.findById(playListReorderItemDto.playlistId)).thenReturn(Optional.of(playlist))
+
+            try {
+                playlistItemService.reorderItemsInPlaylist(playListReorderItemDto)
+            }catch (ex: Exception){
+                Assertions.assertEquals(PlaylistItemNotFoundException::class.java, ex.javaClass)
+            }
+
+            Mockito.verify(playlistRepository, times(1)).findById(playListReorderItemDto.playlistId)
+            Mockito.verify(playlistRepository, times(0)).save(playlistCaptor.capture())
+        }
+
+        @Test
+        fun throwCustomExceptionOnRunTimeEx(){
+
+            whenever(playlistRepository.findById(playListReorderItemDto.playlistId)).thenThrow(RuntimeException())
+
+            try {
+                playlistItemService.reorderItemsInPlaylist(playListReorderItemDto)
+            }catch (ex: Exception){
+                Assertions.assertEquals(FailedToSavePlaylistItemException::class.java, ex.javaClass)
+            }
+
+            Mockito.verify(playlistRepository, times(1)).findById(playListReorderItemDto.playlistId)
+            Mockito.verify(playlistRepository, times(0)).save(playlistCaptor.capture())
         }
     }
 
